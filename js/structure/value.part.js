e1614c890afc42cb61f133af7090c77fb444e008
João Magalhães <joamag@gmail.com>
2012-10-03 17:44:56 +0100

    new value support

--- js/structure/value.part.js
+++ js/structure/value.part.js
@@ -3,16 +3,16 @@
         // sets the jquery matched object
         var matchedObject = this;
 
+        // retrieves the object (type) for the currently
+        // matched object then uses it to contruct the method
+        // name to be used and uses it to retrieve the the
+        // value for the component
         var object = matchedObject.attr("data-object");
+        var method = matchedObject["ux" + object]
+        var value = method ? method.call(matchedObject, "value") : null;
 
-        switch (object) {
-            case "textfield" :
-                return matchedObject.uxtextfield("value");
-                break;
-
-            case "tagfield" :
-                return matchedObject.uxtagfield("value");
-                break;
-        }
+        // returns the just retrived value from the component
+        // to the caller method
+        return value;
     }
 })(jQuery);
