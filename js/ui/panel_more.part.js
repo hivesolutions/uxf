5841d791beac9fee2036aa6391db48cb624c100b
João Magalhães <joamag@gmail.com>
2012-10-04 16:25:36 +0100

    new panel more

---
+++ js/ui/panel_more.part.js
@@ -0,0 +1,63 @@
+/**
+ * jQuery panel more plugin, this jQuery plugin provides the base
+ * infra-structure for the creation of a panel more component.
+ *
+ * @name jquery-select-list.js
+ * @author João Magalhães <joamag@hive.pt>
+ * @version 1.0
+ * @date March 10, 2010
+ * @category jQuery plugin
+ * @copyright Copyright (c) 2010-2012 Hive Solutions Lda.
+ * @license Hive Solutions Confidential Usage License (HSCUL) -
+ *          http://www.hive.pt/licenses/
+ */
+(function($) {
+    jQuery.fn.uxpanelmore = function(options) {
+        // the default values for the panel
+        var defaults = {};
+
+        // sets the default options value
+        var options = options ? options : {};
+
+        // constructs the options
+        var options = jQuery.extend(defaults, options);
+
+        // sets the jquery matched object
+        var matchedObject = this;
+
+        /**
+         * Initializer of the plugin, runs the necessary functions to initialize
+         * the structures.
+         */
+        var initialize = function() {
+            _appendHtml();
+            _registerHandlers();
+        };
+
+        /**
+         * Creates the necessary html for the component.
+         */
+        var _appendHtml = function() {
+            // iterates over each of the matched object to arrange
+            // them according to their options
+            matchedObject.each(function(index, element) {
+                        // retrieves the current element and uses it
+                        // to update ths current arrange structure
+                        var _element = jQuery(this);
+                        _update(_element, options);
+                    });
+        };
+
+        /**
+         * Registers the event handlers for the created objects.
+         */
+        var _registerHandlers = function() {
+        };
+
+        // initializes the plugin
+        initialize();
+
+        // returns the object
+        return this;
+    };
+})(jQuery);
