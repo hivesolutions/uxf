202f548a66ed0cb4ac49735341646648bc2f7f54
joamag <joamag@gmail.com>
2012-10-12 00:02:55 +0100

    tag field bug fixed

--- js/ui/tag_field.part.js
+++ js/ui/tag_field.part.js
@@ -122,7 +122,7 @@
 
                         // checks if the element is visible so that appropriate
                         // visibility operation take place for carrect layout update
-                        var isVisible = _element.is(":visible")
+                        var isVisible = _element.is(":visible");
 
                         // shows the element momentanely so that appropriate width
                         // height measurements may take place
