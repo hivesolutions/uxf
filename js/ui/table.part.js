4acff4912fd792fd1ebf6bf1d62be67160939a62
João Magalhães <joamag@gmail.com>
2012-10-15 12:27:59 +0100

    new default value removal

--- js/ui/table.part.js
+++ js/ui/table.part.js
@@ -211,10 +211,13 @@
                     var _line = _newLine(table, tableBody, options);
 
                     // adds the class default field to the line and register
-                    // for the key down even on it for the removal of the
-                    // default field token
+                    // for the key down (and up) event on it for the removal
+                    // of the default field token
                     _line.addClass("table-default-field");
                     jQuery(".text-field", _line).keydown(function() {
+                                _line.removeClass("table-default-field");
+                            });
+                    jQuery(".text-field", _line).keyup(function() {
                                 _line.removeClass("table-default-field");
                             });
                 }
