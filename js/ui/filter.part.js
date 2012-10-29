9d081ab81a6926ad1ecf32bbdf7553584b97bc83
João Magalhães <joamag@gmail.com>
2012-10-29 19:27:24 +0000

    new showing flag control

--- js/ui/filter.part.js
+++ js/ui/filter.part.js
@@ -2121,6 +2121,10 @@
                 return false;
             }
 
+            // sets the "locking" flag indicating that the sub menu
+            // is already being shown
+            subMenu.data("showing", true);
+
             // retrieves the top and left offset positions
             // for the current element (these are the offset
             // position of it)
@@ -2155,10 +2159,6 @@
             // adds the selected class to the element (button)
             // so that it's highlighted
             element.addClass("selected");
-
-            // sets the "locking" flag indicating that the sub menu
-            // is already being shown
-            subMenu.data("showing", true);
 
             // shows the sub menu with a fade effect
             subMenu.fadeIn(150, function() {
