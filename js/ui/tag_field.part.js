4f8a71d879af1868af94168c71943995bdbd0308
João Magalhães <joamag@hive.pt>
2013-01-13 16:39:38 +0000

    new parent form refactor

--- js/ui/tag_field.part.js
+++ js/ui/tag_field.part.js
@@ -162,7 +162,9 @@
                 // retrieves the containing form
                 var parentForm = _element.parents("form");
 
-                // registers for the submit event
+                // registers for the submit event on the parent
+                // form, in order to be able to create the appropriate
+                // structures for proper submission
                 parentForm.submit(function() {
                             // retrieves the name of the element, this value is
                             // going to be used in the input element to be create
