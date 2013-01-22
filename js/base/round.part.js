fa0ce5da55f9f5c821034e2de84d3c9b1a3faa7d
joamag <joamag@gmail.com>
2013-01-22 19:58:17 +0000

    new round support

---
+++ js/base/round.part.js
@@ -0,0 +1,8 @@
+(function($) {
+    jQuery.uxround = function(value, decimalPlaces) {
+        // rounds the specified value to the
+        // specified number of decimal places
+        var rounder = Math.pow(10, decimalPlaces);
+        return Math.round(value * rounder) / rounder;
+    };
+});
