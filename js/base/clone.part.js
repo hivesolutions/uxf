1974df83a1f865a6ed6791648c954cc895723bc8
joamag <joamag@gmail.com>
2013-01-23 17:01:56 +0000

    new clone support for uxf

---
+++ js/base/clone.part.js
@@ -0,0 +1,32 @@
+(function($) {
+    jQuery.uxclone = function(value) {
+        // in case the provided value is not valid or
+        // in case it's not an object returns the same
+        // object to the caller method
+        if (value == null || typeof value !== "object") {
+            return value;
+        }
+
+        // creates the base copy of the value by using
+        // the same constructor and then iterates over
+        // the various attributes in the value to copy
+        // them into the copy
+        var copy = value.constructor();
+        for (var name in value) {
+            // in case the current value does not have
+            // the current attribute (invalid attribute)
+            // must skip current iteration
+            if (!value.hasOwnProperty(name)) {
+                continue;
+            }
+
+            // copies the original value into the target
+            // copy structure
+            copy[name] = value[name];
+        }
+
+        // returns the resulting copy structure to the
+        // caller method
+        return copy;
+    };
+})(jQuery);
