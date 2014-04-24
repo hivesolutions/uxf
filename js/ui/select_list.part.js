9a656cfd692eacd9dbbaf5eb3ee936bc9a33310d
joamag <joamag@gmail.com>
2014-04-24 16:07:01 +0100

    new year

--- js/ui/select_list.part.js
+++ js/ui/select_list.part.js
@@ -7,7 +7,7 @@
  * @version 1.0
  * @date March 10, 2010
  * @category jQuery plugin
- * @copyright Copyright (c) 2010-2012 Hive Solutions Lda.
+ * @copyright Copyright (c) 2008-2014 Hive Solutions Lda.
  * @license Hive Solutions Confidential Usage License (HSCUL) -
  *          http://www.hive.pt/licenses/
  */
