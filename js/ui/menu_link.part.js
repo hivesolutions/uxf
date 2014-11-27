0fdceda63f39a8fd85b98797d981446b7303e296
joamag <joamag@gmail.com>
2014-11-27 23:31:50 +0000

    new menu link support

--- js/ui/menu_link.part.js
+++ js/ui/menu_link.part.js
@@ -47,6 +47,14 @@
          * Registers the event handlers for the created objects.
          */
         var _registerHandlers = function() {
+            // in case the matched object is not defined
+            // or in case it's an empty list must return
+            // immediatly initialization is not meant to
+            // be run (corruption may occur)
+            if (!matchedObject || matchedObject.length == 0) {
+                return;
+            }
+
             // retrieves the window and body elements
             var _window = jQuery(window);
             var _body = jQuery("body");
