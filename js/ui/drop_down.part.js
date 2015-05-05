a77e4e13dbdd68f0f882365708a76e5151a94d61
joamag <joamag@gmail.com>
2015-05-05 13:50:42 +0100

    better drop down structure

--- js/ui/drop_down.part.js
+++ js/ui/drop_down.part.js
@@ -41,6 +41,14 @@
          * Creates the necessary html for the component.
          */
         var _appendHtml = function() {
+            // in case the matched object is not defined
+            // or in case it's an empty list must return
+            // immediatly initialization is not meant to
+            // be run (corruption may occur)
+            if (!matchedObject || matchedObject.length == 0) {
+                return;
+            }
+
             // creates the upper structure for the drop down, this should
             // include the button part so that it's possible to active the
             // drop down contents using the "usual" manner
@@ -63,19 +71,91 @@
          * Registers the event handlers for the created objects.
          */
         var _registerHandlers = function() {
-            // retrieves the parent drop down container and the button that
-            // is going to be used to active the drop down list
+            // in case the matched object is not defined
+            // or in case it's an empty list must return
+            // immediatly initialization is not meant to
+            // be run (corruption may occur)
+            if (!matchedObject || matchedObject.length == 0) {
+                return;
+            }
+
+            // retrieves the reference to the various elements that are
+            // to be used in the event handling registration
+            var _body = jQuery("body");
             var container = matchedObject.parents(".drop-down-container");
             var button = jQuery(".button-drop-down", container);
+            var elements = jQuery("> li", matchedObject);
+
+            // checks if the drop downk click event is already
+            // registerd in the body and set the variable as
+            // true to avoid further registrations
+            var isRegistered = _body.data("drop_down_click");
+            _body.data("drop_down_click", true);
 
             // registers for the click operation in the drop down button
             // that is going to toggle the current drop down list visibility
-            button.click(function() {
+            button.click(function(event) {
                         var element = jQuery(this);
                         var container = element.parents(".drop-down-container");
                         var dropDown = jQuery(".drop-down", container);
                         _toggle(dropDown, options);
+                        event.stopPropagation();
                     });
+
+            // registers for the click operation in the element so that the
+            // visibility of the curren drop down is hidden
+            elements.click(function(event) {
+                        var element = jQuery(this);
+                        var dropDown = element.parents(".drop-down");
+                        _hide(dropDown, options);
+                        event.stopPropagation();
+                    });
+
+            // registers for the "basic" hide operation of the current
+            // drop down so that the element is shown
+            matchedObject.bind("hide", function() {
+                        var element = jQuery(this);
+                        _hide(element, options);
+                    });
+
+            // register for the key down event in the body,
+            // only in case the registration was not already made
+            !isRegistered && _body.keydown(function(event) {
+                // retrieves the element
+                var element = jQuery(this);
+
+                // retrieves the key value
+                var keyValue = event.keyCode ? event.keyCode : event.charCode
+                        ? event.charCode
+                        : event.which;
+
+                // in case the key that was pressed in not the
+                // escape one there's nothing to be done and so
+                // the control flow is returned immediately
+                if (keyValue != 27) {
+                    return;
+                }
+
+                // retrieves the reference to the complete set of drop down
+                // conatainers that are visible for the current body and then
+                // runs the hide operation for the associated drop down
+                var container = jQuery(".drop-down-container.visible", element);
+                var dropDown = jQuery(".drop-down", container);
+                _hide(dropDown, options);
+            });
+
+            !isRegistered && _body.click(function(event) {
+                // retrieves the reference to the current element, this should
+                // be a top level body element (from dom structure)
+                var element = jQuery(this);
+
+                // retrieves the reference to the complete set of drop down
+                // conatainers that are visible for the current body and then
+                // runs the hide operation for the associated drop down
+                var container = jQuery(".drop-down-container.visible", element);
+                var dropDown = jQuery(".drop-down", container);
+                _hide(dropDown, options);
+            });
         };
 
         var _toggle = function(matchedObject, options) {
@@ -106,6 +186,12 @@
                 // runs the show operation on the currently matched
                 // object so that the proper contents are displayed
                 _show(matchedObject, options);
+                break;
+
+            case "hide" :
+                // runs the hide operation on the currently matched
+                // object so that the proper contents are hidden
+                _hide(matchedObject, options);
                 break;
 
             case "default" :
