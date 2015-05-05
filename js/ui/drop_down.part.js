30d54120f02072cb56b3691caa7fb12566b37fe0
joamag <joamag@gmail.com>
2015-05-05 23:43:33 +0100

    new uxf

--- js/ui/drop_down.part.js
+++ js/ui/drop_down.part.js
@@ -52,7 +52,6 @@
             // creates the upper structure for the drop down, this should
             // include the button part so that it's possible to active the
             // drop down contents using the "usual" manner
-            matchedObject.addClass("menu");
             matchedObject.wrap("<div class=\"drop-down-container\"></div>");
             var container = matchedObject.parents(".drop-down-container");
             container.prepend("<div class=\"button button-drop-down\"></div>");
@@ -70,6 +69,10 @@
                         button.attr("class", buttonClasses + " " + classes);
                         button.removeClass("drop-down");
                     });
+
+            // adds the menu class to the matched object so that it's
+            // possible to manage its visibility as typical menu element
+            matchedObject.addClass("menu");
         };
 
         /**
