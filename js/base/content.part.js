84de680440ca63fd261d44faa12ce8ed4481b19b
joamag <joamag@gmail.com>
2015-10-22 16:39:59 +0100

    small hot patch

--- js/base/content.part.js
+++ js/base/content.part.js
@@ -1,7 +1,8 @@
 jQuery.fn.uxcontent = function(value) {
     var element = jQuery(this);
     var other = element.clone();
-    var children = other.children().remove();
+    var children = other.children();
+    children.remove();
     if (typeof value === "string") {
         other.html(value);
         element.html(children);
