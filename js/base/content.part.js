4ff049c15e9177368d89d1fde0748c7eba1f309f
joamag <joamag@gmail.com>
2015-10-22 16:38:26 +0100

    new content support

---
+++ js/base/content.part.js
@@ -0,0 +1,11 @@
+jQuery.fn.uxcontent = function(value) {
+    var element = jQuery(this);
+    var other = element.clone();
+    var children = other.children().remove();
+    if (typeof value === "string") {
+        other.html(value);
+        element.html(children);
+        element.prepend(value);
+    }
+    return other.html();
+}
