850e0f413e047541c37abe8c823990d623118ab0
joamag <joamag@gmail.com>
2015-10-22 16:22:29 +0100

    number only applies to the text part

--- js/text/number.part.js
+++ js/text/number.part.js
@@ -124,8 +124,9 @@
                 }
 
                 // sets the new value string in the element and
-                // adds the processed class to it
-                _element.html(valueString);
+                // adds the processed class to it, note that only
+                // the text part of the element is changed
+                _element.text(valueString);
                 _element.addClass("processed");
             });
         };
