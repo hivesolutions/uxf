1d47e4cf10a29a525356d54f7b6d0466b6523e9d
joamag <joamag@hive.pt>
2015-11-26 13:14:43 +0000

    initial uxcountry support. Fixes #17

---
+++ js/base/country.part.js
@@ -0,0 +1,20 @@
+(function(jQuery) {
+    jQuery.uxcountry = function(value, format, locale) {
+        var _body = jQuery("body");
+
+        format = format || "iso";
+        locale = locale || _body.attr("data-locale");
+        locale = locale || _body.data("locale");
+        locale = locale || "en-us";
+        var localeC = "en_" + format;
+
+        var COUNTRIES_ISO = jQuery.uxlocale("COUNTRIES", localeC);
+        var index = COUNTRIES_ISO.indexOf(value);
+        if (index == -1) {
+            return value;
+        }
+
+        var COUNTRIES = jQuery.uxlocale("COUNTRIES", locale);
+        return COUNTRIES[index];
+    };
+})(jQuery);
