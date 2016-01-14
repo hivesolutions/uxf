762812d205d5bac2e642fcbf6712dc73de8f5fd4
joamag <joamag@hive.pt>
2016-01-14 17:29:01 +0000

    new safe delta support

--- js/base/round.part.js
+++ js/base/round.part.js
@@ -11,7 +11,20 @@
      */
     var DELTA = Math.pow(10, FLOAT_PRECISION * -1);
 
-    jQuery.uxround = function(value, decimalPlaces, noDecimal, nodelta) {
+    jQuery.uxdelta = function(value) {
+        var integer = Math.abs(parseInt(value / 1));
+        var count = integer == 0 ? 1 : parseInt(Math.log10(integer)) + 1;
+        var places = FLOAT_PRECISION - count;
+        places = places < 1 ? places = 1 : places;
+        var delta = 1 / Math.pow(10, places)
+        return delta
+    };
+
+    jQuery.uxround = function(value, decimalPlaces, noDecimal, nodelta, noSafe) {
+        // calculates the proper delta value to be used for the
+        // roudning calculus taking into account the safe flag
+        var delta = noSafe ? DELTA : jQuery.uxdelta(value);
+
         // verifies if the no decimal flag has been set and
         // in case it was not set creates a new (precise) decimal
         // value from the provided value so that a better rounding
@@ -21,7 +34,7 @@
         // verifies if the extra delta value should be added, in
         // order to avoid any problems with round up, and if that's
         // the case adds the "small" delta value to the value
-        value = nodelta ? value : value + DELTA;
+        value = nodelta ? value : value + delta;
 
         // rounds the specified value to the
         // specified number of decimal places
