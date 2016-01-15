b21ce00679a1ff6a254683dffe7efd02dea2a6b2
João Magalhães <joamag@hive.pt>
2016-01-15 11:07:52 +0000

    small fixes

--- js/base/round.part.js
+++ js/base/round.part.js
@@ -16,8 +16,8 @@
         var count = integer == 0 ? 1 : parseInt(Math.log10(integer)) + 1;
         var places = FLOAT_PRECISION - count;
         places = places < 1 ? places = 1 : places;
-        var delta = 1 / Math.pow(10, places)
-        return delta
+        var delta = 1 / Math.pow(10, places);
+        return delta;
     };
 
     jQuery.uxround = function(value, decimalPlaces, noDecimal, nodelta, noSafe) {
