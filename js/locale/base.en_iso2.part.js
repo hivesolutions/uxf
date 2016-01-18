e3bcf64291e2770ace2e581a153ee8de34f98ece
joamag <joamag@hive.pt>
2016-01-18 12:18:57 +0000

    new country values

--- js/locale/base.en_iso2.part.js
+++ js/locale/base.en_iso2.part.js
@@ -7,19 +7,19 @@
                 "CD", "CR", "HR", "CU", "CY", "CZ", "DK", "DJ", "DM", "DO",
                 "TL", "EC", "EG", "SV", "GQ", "ER", "EE", "ET", "FJ", "FI",
                 "FR", "GA", "GM", "GE", "DE", "GH", "GR", "GD", "GT", "GN",
-                "GW", "GY", "HT", "HN", "HU", "IS", "IN", "ID", "IR", "IQ",
-                "IE", "IL", "IT", "CI", "JM", "JP", "JO", "KZ", "KE", "KI",
-                "KP", "KR", "XZ", "KW", "KG", "LA", "LV", "LB", "LS", "LR",
-                "LY", "LI", "LT", "LU", "MK", "MG", "MW", "MY", "MV", "ML",
-                "MT", "MH", "MR", "MU", "MX", "FM", "MD", "MC", "MN", "ME",
-                "MA", "MZ", "MM", "NA", "NR", "NP", "NL", "NZ", "NI", "NE",
-                "NG", "NO", "OM", "PK", "PW", "PA", "PG", "PY", "PE", "PH",
-                "PL", "PT", "QA", "RO", "RU", "RW", "KN", "LC", "VC", "WS",
-                "SM", "ST", "SA", "SN", "RS", "SC", "SL", "SG", "SK", "SI",
-                "SB", "SO", "ZA", "ES", "LK", "SD", "SR", "SZ", "SE", "CH",
-                "SY", "TW", "TJ", "TZ", "TH", "TG", "TO", "TT", "TN", "TR",
-                "TM", "TV", "UG", "UA", "AE", "GB", "US", "UY", "UZ", "VU",
-                "VA", "VE", "VN", "YE", "ZM", "ZW"]
+                "GW", "GY", "HT", "HN", "HK", "HU", "IS", "IN", "ID", "IR",
+                "IQ", "IE", "IL", "IT", "CI", "JM", "JP", "JO", "KZ", "KE",
+                "KI", "KP", "KR", "XZ", "KW", "KG", "LA", "LV", "LB", "LS",
+                "LR", "LY", "LI", "LT", "LU", "MO", "MK", "MG", "MW", "MY",
+                "MV", "ML", "MT", "MH", "MR", "MU", "MX", "FM", "MD", "MC",
+                "MN", "ME", "MA", "MZ", "MM", "NA", "NR", "NP", "NL", "NZ",
+                "NI", "NE", "NG", "NO", "OM", "PK", "PW", "PA", "PG", "PY",
+                "PE", "PH", "PL", "PT", "QA", "RO", "RU", "RW", "KN", "LC",
+                "VC", "WS", "SM", "ST", "SA", "SN", "RS", "SC", "SL", "SG",
+                "SK", "SI", "SB", "SO", "ZA", "ES", "LK", "SD", "SR", "SZ",
+                "SE", "CH", "SY", "TW", "TJ", "TZ", "TH", "TG", "TO", "TT",
+                "TN", "TR", "TM", "TV", "UG", "UA", "AE", "GB", "US", "UY",
+                "UZ", "VU", "VA", "VE", "VN", "YE", "ZM", "ZW"]
     }
 
     jQuery.uxloadbundle(BUNDLE_EN_ISO2, "en-iso2")
