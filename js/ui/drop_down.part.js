54e41c894fa8394ba804f12f6f82bb88381b0f8a
joamag <joamag@hive.pt>
2016-01-29 18:39:54 +0000

    better way of checking visibility in drop down

--- js/ui/drop_down.part.js
+++ js/ui/drop_down.part.js
@@ -362,7 +362,7 @@
         var _toggle = function(matchedObject, options) {
             // verifies the current visibility of the object and then uses
             // the value to decide the operation to be performed
-            var isVisible = matchedObject.is(":visible");
+            var isVisible = matchedObject.hasClass("active");
             if (isVisible) {
                 _hide(matchedObject, options);
             } else {
