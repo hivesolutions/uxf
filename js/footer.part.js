955c746d54895a3c5dc51862883d13f5d3395e7c
joamag <joamag@hive.pt>
2016-04-15 19:32:05 +0100

    better footer validation

--- js/footer.part.js
+++ js/footer.part.js
@@ -1,14 +1,31 @@
 // registers for the ready event in
 // the body element
 jQuery(document).ready(function() {
-    // retrieves the base element to run
-    // the apply (contain ux class)
+    // retrieves the base element to run the apply
+    // operation (contains ux class)
     var elements = jQuery(".ux");
+
+    // creates a function that is capable of delaying
+    // the apply operation on a certain element, note
+    // the proper ready validation (height exists) is
+    // going to be applied to the target elements
+    var delay = function(element, timeout) {
+        timeout = timeout || 0;
+        setTimeout(function() {
+            var isReady = element.height() > 0;
+            if (!isReady) {
+                delay(element, 25);
+                return;
+            }
+            elements.uxapply();
+        }, timeout);
+    };
 
     // applies the ux to the elements delaying it's
     // execution until the next tick, offering
     // a way of handling body registration before
-    setTimeout(function() {
-        elements.uxapply();
+    elements.each(function(index, element) {
+        var _element = jQuery(this);
+        delay(_element)
     });
 });
