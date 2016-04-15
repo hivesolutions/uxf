41ac4f3dbf84c3857d579da7c225361acb1b3819
joamag <joamag@gmail.com>
2016-04-15 21:29:23 +0100

    new responsive support

--- js/structure/responsive.part.js
+++ js/structure/responsive.part.js
@@ -168,7 +168,7 @@
         // as some browsers don't trigger the resize event correctly
         !isRegistered && setInterval(function() {
             watch();
-        }, 250);
+        }, 1000);
 
         // measures the current pixel properties of the screen
         // and populates the proper structures
