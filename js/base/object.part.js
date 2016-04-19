7f3789f878ae6376f53af2111132568de85e83f3
joamag <joamag@hive.pt>
2016-04-19 10:43:21 +0100

    new code

--- js/base/object.part.js
+++ js/base/object.part.js
@@ -33,7 +33,7 @@
             // be a top one and the proper attribute is added
             var parents = _element.parents("[data-top]");
             var isTop = parents.length == 0;
-            isTop && _element.addAttr("data-top")
+            isTop && _element.attr("data-top")
 
             // retrieves the complete set of children elements
             // marked as top and removes such association, as
