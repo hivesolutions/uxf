2e060ea8d1ad98cd6bf0f41e0b5b52e2739b42ec
joamag <joamag@gmail.com>
2016-05-09 22:02:14 +0100

    new loading and loaded tests

--- js/ui/image_lazy.part.js
+++ js/ui/image_lazy.part.js
@@ -91,6 +91,8 @@
 
             !isRegistered && _window.scroll(function() {
                 var imagesLazy = jQuery(".image-lazy", _body);
+                imagesLazy = imagesLazy.not(":loading");
+                imagesLazy = imagesLazy.not(":loaded");
                 imagesLazy.each(function(index, element) {
                     var _element = jQuery(this);
                     updateState(_element);
@@ -99,11 +101,23 @@
 
             !isRegistered && _window.resize(function() {
                 var imagesLazy = jQuery(".image-lazy", _body);
+                imagesLazy = imagesLazy.not(":loading");
+                imagesLazy = imagesLazy.not(":loaded");
                 imagesLazy.each(function(index, element) {
                     var _element = jQuery(this);
                     updateState(_element);
                 });
             });
+
+            !isRegistered && setInterval(function() {
+                var imagesLazy = jQuery(".image-lazy", _body);
+                imagesLazy = imagesLazy.not(":loading");
+                imagesLazy = imagesLazy.not(":loaded");
+                imagesLazy.each(function(index, element) {
+                    var _element = jQuery(this);
+                    updateState(_element);
+                });
+            }, 250);
         };
 
         var updateState = function(element) {
