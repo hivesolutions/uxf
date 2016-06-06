d5ad8875739ed2887f80db510f105b8a5146ae4e
joamag <joamag@hive.pt>
2016-06-06 10:33:28 +0100

    small code fix

--- js/ui/tag_field.part.js
+++ js/ui/tag_field.part.js
@@ -523,7 +523,7 @@
             // field associated with the tag field, note that this
             // operation is only performed in case the no width flag
             // is not set (conditional execution)
-            !noWidth && !autoWidth && textField.css("width", width + "px");
+            !noWidth && !autoWidth && textField.width("width", width);
             !noWidth && textField.css("padding-left", lineWidth + "px");
         };
 
