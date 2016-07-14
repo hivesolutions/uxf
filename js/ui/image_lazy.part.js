748c6c940698812dcd99318ab0554e96221c1782
joamag <joamag@gmail.com>
2016-07-14 15:11:24 +0100

    new secure lazy loading of images

--- js/ui/image_lazy.part.js
+++ js/ui/image_lazy.part.js
@@ -114,6 +114,16 @@
                     updateState(_element);
                 });
             });
+
+            !isRegistered && setInterval(function() {
+                var imagesLazy = jQuery(".image-lazy", _body);
+                imagesLazy = imagesLazy.not(".loading");
+                imagesLazy = imagesLazy.not(".loaded");
+                imagesLazy.each(function(index, element) {
+                    var _element = jQuery(this);
+                    updateState(_element);
+                });
+            }, 250);
         };
 
         var updateState = function(element) {
