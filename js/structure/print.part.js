5cf2eb6e252aac57de12d3f0a4ff685b47579344
joamag <joamag@gmail.com>
2017-07-25 11:43:44 +0100

    initial support for delay in printing

--- js/structure/print.part.js
+++ js/structure/print.part.js
@@ -25,15 +25,25 @@
          * Creates the necessary html for the component.
          */
         var _appendHtml = function() {
-            // in case the matched object is empty
+            // in case the matched object is empty, returns immediately
+            // as there's nothing pending to be done
             if (!matchedObject || matchedObject.length == 0) {
-                // returns immediately
                 return;
             }
 
-            // prints the current document
-            // to the printer
-            window.print();
+            // tries to retrieve the amount of delay to be applied
+            // before running the print operation, this allows for
+            // waiting for the rendering of the data, the provided
+            // value should be defined in milliseconds
+            var delay = matchedObject.attr("data-delay") || 0;
+            delay = parseInt(delay);
+
+            // prints the current document to the printer, this
+            // should be a blocking operation and may block the
+            // current browser interaction
+            setTimeout(function() {
+                window.print();
+            }, delay);
         };
 
         /**
