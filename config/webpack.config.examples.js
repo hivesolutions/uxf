442f9fc921dfa02a263f708de860d4d867da2c90
joamag <joamag@hive.pt>
2018-05-29 18:54:13 +0100

    better example code

--- config/webpack.config.examples.js
+++ config/webpack.config.examples.js
@@ -1,8 +1,31 @@
+const path = require("path");
+
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
 const config = require("./webpack.config.full");
 
 config.entry = "./vue/examples";
 config.output.filename = "vue-uxf.examples.min.js?[hash]";
+config.devServer = {
+    contentBase: path.join(__dirname, "../dist"),
+    compress: false,
+    port: 3000,
+    stats: "minimal",
+    hot: true
+};
 
-delete config.externals.vue;
+config.plugins.push(
+    new HtmlWebpackPlugin({
+        title: "Vue UXF",
+        template: "examples/vue/index.html",
+        cache: false,
+        minify: {
+            removeComments: true,
+            collapseWhitespace: true,
+            conservativeCollapse: false,
+            preserveLineBreaks: false
+        }
+    })
+);
 
 module.exports = config;
