bc09ebcc0daf381006ccc84accd56222337a9ad2
joamag <joamag@hive.pt>
2018-05-29 12:48:37 +0100

    initial vue files

---
+++ vue/button.vue
@@ -0,0 +1,29 @@
+<template>
+<div class="button button-color" v-bind:class="{ visible: isVisible }">
+</div>
+</template>
+
+<style scoped>
+</style>
+
+<script>
+import Vue from "vue";
+
+export const UxButton = Vue.component("uxbutton", {
+    data: function() {
+        return {
+            isVisible: false
+        };
+    },
+    methods: {
+        show: function() {
+            this.isVisible = true;
+        },
+        hide: function() {
+            this.isVisible = false;
+        }
+    }
+});
+
+export default UxButton;
+</script>
