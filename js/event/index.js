43de7e38a2b22ce50aa37f28844a3efd2e6053b7
joamag <joamag@gmail.com>
2018-06-05 22:41:16 +0100

    new event values

---
+++ js/event/index.js
@@ -0,0 +1,7 @@
+const animend = require("./animend.part");
+const destroyed = require("./destroyed.part");
+const transend = require("./transend.part");
+
+Object.assign(module.exports, animend);
+Object.assign(module.exports, destroyed);
+Object.assign(module.exports, transend);
--- js/index.js
+++ js/index.js
@@ -3,6 +3,7 @@ const attr = require("./attr");
 const base = require("./base");
 const data = require("./data");
 const error = require("./error");
+const event = require("./event");
 const ui = require("./ui");
 
 Object.assign(module.exports, async);
@@ -10,6 +11,7 @@ Object.assign(module.exports, attr);
 Object.assign(module.exports, base);
 Object.assign(module.exports, data);
 Object.assign(module.exports, error);
+Object.assign(module.exports, event);
 Object.assign(module.exports, ui);
 
 module.exports.VERSION = "__VERSION__";
