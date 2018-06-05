60e9a0f2d5c86bdb4908888e4ce64b3e5a7d92df
joamag <joamag@gmail.com>
2018-06-05 21:16:10 +0100

    new import

--- js/index.js
+++ js/index.js
@@ -1,11 +1,13 @@
 const async = require("./async");
 const attr = require("./attr");
 const base = require("./base");
+const data = require("./data");
 const ui = require("./ui");
 
 Object.assign(module.exports, async);
 Object.assign(module.exports, attr);
 Object.assign(module.exports, base);
+Object.assign(module.exports, data);
 Object.assign(module.exports, ui);
 
 module.exports.VERSION = "__VERSION__";
