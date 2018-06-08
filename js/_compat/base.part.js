e4751b0d0be14399d3ebe5fbf9c2ef8c1811fa10
joamag <joamag@hive.pt>
2018-06-08 15:24:20 +0100

    small fixes

--- js/_compat/base.part.js
+++ js/_compat/base.part.js
@@ -1,5 +1,5 @@
 var jquery = require("jquery");
-if (typeof __webpack_require__ !== "function") { // eslint-disable-line camelcase
+if (typeof __webpack_require__ === "undefined") { // eslint-disable-line camelcase
     var jsdom = require("jsdom");
     global.dom = global.dom || new jsdom.JSDOM("");
     global.jQuery = global.jQuery || jquery(global.dom.window);
