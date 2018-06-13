183c418e24581f4971058a3ee3a43a3d1bc6cdb1
joamag <joamag@hive.pt>
2018-06-13 18:37:54 +0100

    simplified eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,5 @@
 module.exports = {
     "extends": [
-        "standard",
-        "plugin:vue/essential"
-    ],
-    "plugins": ["mocha", "vue"],
-    "rules": {
-        "indent": ["warn", 4, {
-            SwitchCase: 1
-        }],
-        "quotes": ["error", "double"],
-        "semi": ["error", "always"],
-        "space-before-function-paren": ["error", {
-            "anonymous": "never",
-            "named": "never",
-            "asyncArrow": "always"
-        }],
-        "mocha/no-exclusive-tests": "error",
-        "no-debugger": "warn",
-        "brace-style": "off",
-        "no-useless-escape": "off",
-        "no-mixed-operators": "off",
-        "operator-linebreak": "off",
-        "standard/no-callback-literal": "off",
-        "standard/computed-property-even-spacing": "off"
-    },
-    "env": {
-        "jasmine": true
-    }
+        "hive"
+    ]
 };
